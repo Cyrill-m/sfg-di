6c1791fbb608eddfa4fa991112ccafd8f411790f
Cyrill-m <Cyrill-m@mail.ru>
2021-09-15 17:28:16 +0300

    Adding Spring DI

--- src/main/java/by/mkstudio/sfgdi/SfgDiApplication.java
+++ src/main/java/by/mkstudio/sfgdi/SfgDiApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import by.mkstudio.sfgdi.controllers.ConstructorInjectedController;
 import by.mkstudio.sfgdi.controllers.MyController;
+import by.mkstudio.sfgdi.controllers.PropertyInjectedController;
+import by.mkstudio.sfgdi.controllers.SetterInjectedController;
 
 @SpringBootApplication
 public class SfgDiApplication {
@@ -15,6 +18,18 @@ public class SfgDiApplication {
 		MyController myController = (MyController) ctx.getBean("myController");
 		String greeting = myController.sayHello();
 		System.out.println(greeting);
+		
+		System.out.println("----- Property ------");
+		PropertyInjectedController piController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(piController.getGreeting());
+		
+		System.out.println("----- Setter ------");
+		SetterInjectedController siController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(siController.getGreeting());
+		
+		System.out.println("----- Constructor ------");
+		ConstructorInjectedController ciController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(ciController.getGreeting());
 	}
 
 }
