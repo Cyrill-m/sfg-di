769f6d6f37b135eb2f6043cf175ceeee35b524c0
Cyrill-m <Cyrill-m@mail.ru>
2021-09-17 12:27:53 +0300

    Add Primary Annotation

--- src/main/java/by/mkstudio/sfgdi/controllers/MyController.java
+++ src/main/java/by/mkstudio/sfgdi/controllers/MyController.java
@@ -2,12 +2,20 @@ package by.mkstudio.sfgdi.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import by.mkstudio.sfgdi.services.PrimaryGreetingService;
+
 @Controller
 public class MyController {
+	
+	private final PrimaryGreetingService greetingService;
+	
+	public MyController(PrimaryGreetingService greetingService) {
+		this.greetingService = greetingService;
+	}
 
 	public String sayHello() {
-		System.out.println("Hello World!!!");
+		//System.out.println("Hello World!!!");
 		
-		return "Hi Folks!";
+		return greetingService.sayGreeting();
 	}
 }
